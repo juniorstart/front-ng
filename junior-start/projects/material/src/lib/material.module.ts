84216c1ff8e3efc68e1829696439385cad2d422b
Grzesiek <grzegorzociepka09@gmail.com>
2019-09-08 13:45:35 +0200

    added material-design lib

---
+++ junior-start/projects/material/src/lib/material.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { MaterialComponent } from './material.component';
+import  {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonToggleModule
+}  from  '@angular/material';
+
+
+
+@NgModule({
+  declarations: [MaterialComponent],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonToggleModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonToggleModule
+  ]
+})
+export class MaterialModule { }
---
+++ junior-start/projects/material/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of material
+ */
+
+export { MaterialModule } from './lib/material.module';
