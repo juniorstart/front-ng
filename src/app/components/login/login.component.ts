2ff5db541b2f0497a8774afaf4fe31253fc4f037
Grzesiek <grzegorzociepka09@gmail.com>
2020-10-28 18:43:18 +0100

    update login validation form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { ToastrService } from 'ngx-toastr';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Login } from 'src/app/interfaces/login.interface';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from 'src/app/services/authentication.service';
+import {ToastrService} from 'ngx-toastr';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {Login} from 'src/app/interfaces/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -12,36 +12,39 @@ import { Login } from 'src/app/interfaces/login.interface';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthenticationService,private toastr: ToastrService) { }
+  _loginForm: FormGroup;
+  _userLogin: Login;
 
-  _loginForm:FormGroup;
-  _userLogin:Login;
+  constructor(private router: Router, private authService: AuthenticationService, private toastr: ToastrService) {
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     this._loginForm = new FormGroup({
-      login: new FormControl([null], [Validators.required, Validators.minLength(6)]),
-      password: new FormControl([null], [Validators.required, Validators.minLength(6)])
+      login: new FormControl([null], [Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z].*')]),
+      password: new FormControl([null], [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).*')])
     });
 
     this._userLogin = {} as Login;
   }
 
   login() {
+    if (this._loginForm.valid) {
+      this.authService.login(Object.assign(this._userLogin, this._loginForm.value))
+        .subscribe(result => {
+          // Store the token
+          this.authService.setToken(JSON.stringify(result));
+          this.toastr.success('Logged succesfully');
+          this.router.navigate(['']);
+        }, error => {
+          if (error.error.errors) {
+            (Object.values(error.error.errors) as string[]).reduce((acc, val) => acc.concat(val), []).forEach(msg => this.toastr.error(msg));
+          } else {
+            this.toastr.error(error.error.ErrorMessage);
+          }
 
-    this.authService.login(Object.assign(this._userLogin, this._loginForm.value))
-      .subscribe(result => {
-        // Store the token
-        this.authService.setToken(JSON.stringify(result));
-        this.toastr.success('Logged succesfully');
-        this.router.navigate(['']);
-      },error  => {
-        if (error.error.errors) {
-          (Object.values(error.error.errors) as string[]).reduce((acc, val) => acc.concat(val), []).forEach(msg => this.toastr.error(msg));
-        } else {
-          this.toastr.error(error.error.ErrorMessage);
-        }
+        });
+    }
 
-      });
   }
 
 }
--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { MaterialModule } from 'projects/material/src/public-api';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -9,7 +10,8 @@ import { MaterialModule } from 'projects/material/src/public-api';
   imports: [
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ],
   exports:[LoginComponent]
 })
